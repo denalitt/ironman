bfbbad5d40640dc00c306aa2ec2054d53e982f41
root <root@denaliA-ubuntu0.juniper.net>
2017-05-23 01:53:44 -0400

    add telstra_snappy.py

---
+++ library/telstra_snappy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+DOCUMENTATION='''
+---
+DOCUMENTATION GOES HERE
+'''
+
+EXAMPLE='''
+EXAMPEL GOES HERE
+'''
+
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec=dict(
+            device=dict(required=True, type='str'),
+            username=dict(required=True, type='str'),
+            passwd=dict(required=True, type='str'),
+            platform=dict(required=True, type='str'),
+            tests=dict(required=True, type='list'),
+        ),
+        mutually_exclusive=(
+            [],
+        ),
+        supports_check_mode=False
+    )
+
+    results = {}
+    results['response'] = []
+    results['response']['msg'] = "ALL GOOD HERE"
+
+    module.exit_json(**results)
+
+
+from ansible.module_utils.basic import *
+if __name__ == "__main__":
+    main()
